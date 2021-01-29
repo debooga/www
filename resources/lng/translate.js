9578b8ef5b143b550f078ce49236db182669cbfd
Vatqin <78079338+Vatqin@users.noreply.github.com>
2021-01-29 14:31:16 +0100

    working_transtating

---
+++ resources/lng/translate.js
@@ -0,0 +1,30 @@
+let languageElements = {
+	en: 
+	{
+		test: "T   ES  t ",
+		menu_one: "Main page"
+	},
+	pl:
+	{
+		test: "Test",
+		menu_one: "Strona główna"
+	}
+}
+
+let languageTag = document.documentElement.lang
+let changeToEnglishButton = document.getElementById("changeToEnglish")
+let changeToPolishButton = document.getElementById("changeToPolish")
+ 
+function changeToEnglish() {
+    document.documentElement.lang = "en"
+    document.getElementById("test").innerHTML = languageElements.en.test
+	document.getElementById("menu_one").innerHTML = languageElements.en.menu_one
+}
+ 
+function changeToPolish() {
+    document.documentElement.lang = "pl"
+    document.getElementById("test").innerHTML = languageElements.pl.test
+	document.getElementById("menu_one").innerHTML = languageElements.pl.menu_one
+}
+changeToEnglishButton.addEventListener("click", changeToEnglish)
+changeToPolishButton.addEventListener("click", changeToPolish)
