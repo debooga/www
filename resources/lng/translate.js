219035e7ab483ebe47f4740085c73035cbffed98
Vatqin <78079338+Vatqin@users.noreply.github.com>
2021-01-29 17:51:54 +0100

    Update translate.js

--- resources/lng/translate.js
+++ resources/lng/translate.js
@@ -16,14 +16,14 @@ let changeToEnglishButton = document.getElementById("changeToEnglish")
 let changeToPolishButton = document.getElementById("changeToPolish")
  
 function changeToEnglish() {
-    document.documentElement.lang = "en"
-    document.getElementById("test").innerHTML = languageElements.en.test
+	document.documentElement.lang = "en"
+	document.getElementById("test").innerHTML = languageElements.en.test
 	document.getElementById("menu_one").innerHTML = languageElements.en.menu_one
 }
  
 function changeToPolish() {
-    document.documentElement.lang = "pl"
-    document.getElementById("test").innerHTML = languageElements.pl.test
+	document.documentElement.lang = "pl"
+	document.getElementById("test").innerHTML = languageElements.pl.test
 	document.getElementById("menu_one").innerHTML = languageElements.pl.menu_one
 }
 changeToEnglishButton.addEventListener("click", changeToEnglish)
