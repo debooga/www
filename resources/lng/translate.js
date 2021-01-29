6964f616c20a9c33ddb9cd8cf310e76bdccd28d7
Vatqin <78079338+Vatqin@users.noreply.github.com>
2021-01-29 19:38:40 +0100

    Trs wrk agn agn

--- resources/lng/translate.js
+++ resources/lng/translate.js
@@ -1,15 +1,16 @@
 let languageElements = {
 	en: 
 	{
-		menu_op_one: "Main page",
-		test: "test"
+		menu_one: "Main page",
+		contact: "Contact"
 	},
 	pl:
 	{
-		menu_op_one: "Strona główna",
-		test: "TEST"
+		menu_one: "Strona główna",
+		contact: "Kontakt"
 	}
 }
+
 
 
 let languageTag = document.documentElement.lang
@@ -18,12 +19,14 @@ let changeToPolishButton = document.getElementById("changeToPolish")
  
 function changeToEnglish() {
 	document.documentElement.lang = "en"
-	document.getElementById("test").innerHTML = languageElements.en.test
+	document.getElementById("menu_one").innerHTML = languageElements.en.menu_one
+	document.getElementById("contact").innerHTML = languageElements.en.contact
 }
  
 function changeToPolish() {
 	document.documentElement.lang = "pl"
-	document.getElementById("test").innerHTML = languageElements.pl.test
+	document.getElementById("menu_one").innerHTML = languageElements.pl.menu_one
+	document.getElementById("contact").innerHTML = languageElements.pl.contact
 }
 changeToEnglishButton.addEventListener("click", changeToEnglish)
 changeToPolishButton.addEventListener("click", changeToPolish)
