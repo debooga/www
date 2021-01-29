880321da2c0f49068b77dacb6f2b2982a4f8b7ee
Vatqin <78079338+Vatqin@users.noreply.github.com>
2021-01-29 18:14:52 +0100

    google->#

--- resources/lng/translate.js
+++ resources/lng/translate.js
@@ -1,13 +1,11 @@
 let languageElements = {
 	en: 
 	{
-		test: "T   ES  t ",
-		menu_one: "Main page"
+		menu_op_one: "Main page"
 	},
 	pl:
 	{
-		test: "Test",
-		menu_one: "Strona główna"
+		menu_op_one: "Strona główna"
 	}
 }
 
@@ -17,14 +15,12 @@ let changeToPolishButton = document.getElementById("changeToPolish")
  
 function changeToEnglish() {
 	document.documentElement.lang = "en"
-	document.getElementById("test").innerHTML = languageElements.en.test
-	document.getElementById("menu_one").innerHTML = languageElements.en.menu_one
+	document.getElementById("menu_op_one").innerHTML = languageElements.en.menu_op_one
 }
  
 function changeToPolish() {
 	document.documentElement.lang = "pl"
-	document.getElementById("test").innerHTML = languageElements.pl.test
-	document.getElementById("menu_one").innerHTML = languageElements.pl.menu_one
+	document.getElementById("menu_op_one").innerHTML = languageElements.pl.menu_op_one
 }
 changeToEnglishButton.addEventListener("click", changeToEnglish)
 changeToPolishButton.addEventListener("click", changeToPolish)
