4b665b1aff70efbf30f599ce12b62ce3a553ea3b
Deerrorer <70140437+Deerrorer@users.noreply.github.com>
2021-01-30 13:31:22 +0100

    improved smooth scrolling

--- scripts/scrollbutton.js
+++ scripts/scrollbutton.js
@@ -14,6 +14,6 @@
   
   // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
-    document.body.scrollTop = 1;
-    document.documentElement.scrollTop = 1;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
