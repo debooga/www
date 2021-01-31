2065a28b2413ab88dab8568348cf2f0d216c5912
Deerrorer <70140437+Deerrorer@users.noreply.github.com>
2021-01-31 12:34:39 +0100

    improved theme icons

--- scripts/themechange.js
+++ scripts/themechange.js
@@ -17,16 +17,19 @@ function toggleTheme()
         setTheme('light_theme');
         changeIcon.className = 'iconslight'
         zoomChangeIcon.className = 'iconslight'
+        changeIcon.innerHTML = "accessible"
     } else {
         if (localStorage.getItem('theme') === 'light_theme')
         {
             setTheme('access_theme');
             changeIcon.className = 'iconsaccess'
             zoomChangeIcon.className = 'iconsaccess'
+            changeIcon.innerHTML = "brightness_3"
         } else {
             setTheme('dark_theme');
             changeIcon.className = 'iconsdark'
             zoomChangeIcon.className = 'iconsdark'
+            changeIcon.innerHTML = "brightness_5"
         }
     }
 }
