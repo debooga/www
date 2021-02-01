63230616552e363a8d9bac9c79d866270929170d
debooga <debooga@github.com>
2021-02-01 12:44:48 +0100

    Improved font resizing script (not finished)

--- scripts/zoomchange.js
+++ scripts/zoomchange.js
@@ -11,22 +11,26 @@ function toggleZoom()
 {
     if (localStorage.getItem('zoom') === 'zoomIn') {
         setZoom('zoomOut');
-
-        var el = document.getElementById('bar');
-        var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
-        var fontSize = parseFloat(style);
+    
+        var el = document.querySelectorAll("span");
+        for (var i= 0; i < el.length; i++) {
+            var style = window.getComputedStyle(el[i], null).getPropertyValue('font-size');
+            var fontSize = parseFloat(style);
+            el[i].style.fontSize = (fontSize - 6) + 'px';
+        }
 
         zoomIcon.innerHTML = "zoom_in"
-        el.style.fontSize = (fontSize - 6) + 'px';
     } else {
         setZoom('zoomIn');
 
-        var el = document.getElementById('bar');
-        var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
-        var fontSize = parseFloat(style);
+        var el = document.querySelectorAll("span");
+        for (var i= 0; i < el.length; i++) {
+            var style = window.getComputedStyle(el[i], null).getPropertyValue('font-size');
+            var fontSize = parseFloat(style);
+            el[i].style.fontSize = (fontSize + 6) + 'px';
+        }
 
         zoomIcon.innerHTML = "zoom_out"
-        el.style.fontSize = (fontSize + 6) + 'px';
     }
 }
 
