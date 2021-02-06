ddc59c9ad0a2c3048e13c1aab7c142788c11dd7e
debooga <debooga@github.com>
2021-02-06 14:06:33 +0100

    Largely simplified translate.js

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,39 +1,29 @@
-let languageTag = document.documentElement.lang
-let changeToEnglishButton = document.getElementById("changeToEnglish")
-let changeToPolishButton = document.getElementById("changeToPolish")
+window.onload = function fetchData() {
+    fetch("https://raw.githubusercontent.com/debooga/www/main/resources/lng/en.json")
+    .then(response => response.json())
+    .then(data => {
+        englishData = data
+    })
 
-fetch("https://raw.githubusercontent.com/debooga/www/main/resources/lng/en.json")
-.then(response => response.json())
-.then(data => {
-	function changeToEnglish() {
-        for (var x in data) {
-            for (var n = 0; n < x.length; n++) {
-                var keys = Object.keys(data)
-                var el = document.getElementById(keys[n])
-                if(el !== null && typeof el !== 'undefined') {
-                    el.innerHTML = data[keys[n]]
-                }
-            }
-        }
-        document.documentElement.lang = "en"
-	}
-	changeToEnglishButton.addEventListener("click", changeToEnglish)
-})
+    fetch("https://raw.githubusercontent.com/debooga/www/main/resources/lng/pl.json")
+    .then(response => response.json())
+    .then(data => {
+        polishData = data
+    })
+}
 
-fetch("https://raw.githubusercontent.com/debooga/www/main/resources/lng/pl.json")
-.then(response => response.json())
-.then(data => {
-	function changeToPolish() {
-        for (var x in data) {
-            for (var n = 0; n < x.length; n++) {
-                var keys = Object.keys(data)
-                var el = document.getElementById(keys[n])
-                if (el !== null && typeof el !== 'undefined') {
-                    el.innerHTML = data[keys[n]]
-                }
-            }
+function translate(langData, lang) {
+    for (var x in langData) {
+        for (var n = 0; n < x.length; n++) {
+            var keys = Object.keys(langData)
+            var el = document.getElementById(keys[n])
+            if(el !== null && typeof el !== 'undefined')
+                el.innerHTML = langData[keys[n]]
         }
-		document.documentElement.lang = "pl"
-	}
-	changeToPolishButton.addEventListener("click", changeToPolish)
-})
+    }
+    document.documentElement.lang = lang
+}
+
+document.getElementById("changeToEnglish").addEventListener("click", function() { translate(englishData, "en") })
+document.getElementById("changeToPolish").addEventListener("click", function(){ translate(polishData, "pl") })
+
