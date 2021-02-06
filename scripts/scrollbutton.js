183ee924dd49ce266cdd8d58862f3008e344d482
debooga <debooga@github.com>
2021-02-06 16:01:29 +0100

    Simplified scrollbutton.js

--- scripts/scrollbutton.js
+++ scripts/scrollbutton.js
@@ -1,19 +1,14 @@
-  //Get the button
-  var mybutton = document.getElementById("scrollup");
-  
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
+window.onscroll = function() { scrollFunction() }
+
+function scrollFunction() {
     if (document.body.scrollTop > 8900 || document.documentElement.scrollTop > 8900 ) {
         anime({ targets: '#scrollup', scale: 1 })
     } else {
         anime({ targets: '#scrollup', scale: 0 })
     }
-  }
+}
   
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+function topFunction() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
