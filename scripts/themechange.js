e58232e0f4a47e37d0071a10984dd5bdbc241a63
Deerrorer <70140437+Deerrorer@users.noreply.github.com>
2021-02-07 17:38:45 +0100

    simplified icons change

--- scripts/themechange.js
+++ scripts/themechange.js
@@ -14,20 +14,20 @@ function setTheme(themeName)
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'dark_theme') {
         setTheme('light_theme');
-        changeIcon.className = 'iconslight'
-        zoomChangeIcon.className = 'iconslight'
+        changeIcon.className = 'icons2'
+        zoomChangeIcon.className = 'icons2'
         changeIcon.innerHTML = "accessible"
     } else {
         if (localStorage.getItem('theme') === 'light_theme')
         {
             setTheme('access_theme');
-            changeIcon.className = 'iconsaccess'
-            zoomChangeIcon.className = 'iconsaccess'
+            changeIcon.className = 'icons2'
+            zoomChangeIcon.className = 'icons2'
             changeIcon.innerHTML = "brightness_4"
         } else {
             setTheme('dark_theme');
-            changeIcon.className = 'iconsdark'
-            zoomChangeIcon.className = 'iconsdark'
+            changeIcon.className = 'icons2'
+            zoomChangeIcon.className = 'icons2'
             changeIcon.innerHTML = "brightness_5"
         }
     }
